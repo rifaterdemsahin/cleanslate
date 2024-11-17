bbbb717075239496cc37bd0c71e0d22562dc018f
Erdem <rifaterdemsahin@gmail.com>
2024-11-17 10:54:33 +0000

    Codespaces
    mvp

---
+++ Symbols/processgitrepo.py
@@ -0,0 +1,7 @@
+from git import Repo
+
+# Load repo
+repo_path = "repo_folder"  # Replace with your repo folder name
+repo = Repo(repo_path)
+
+print("Repo loaded successfully!")  # Ensure the repo is loaded

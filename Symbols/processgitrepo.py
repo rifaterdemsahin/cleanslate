d951ae9a3b160c837535e1f9f2fd76e805c035e0
Erdem <rifaterdemsahin@gmail.com>
2024-11-17 10:55:55 +0000

    Codespaces
    mvp

--- Symbols/processgitrepo.py
+++ Symbols/processgitrepo.py
@@ -1,7 +1,7 @@
 from git import Repo
 
 # Load repo
-repo_path = "repo_folder"  # Replace with your repo folder name
+repo_path = "nginx"  # Replace with your repo folder name
 repo = Repo(repo_path)
 
 print("Repo loaded successfully!")  # Ensure the repo is loaded
